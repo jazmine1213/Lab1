94cd2cd238c4f9a1876127f6b85a03d7ac6c951f
JasmineB <j.mine1213@gmail.com>
2020-12-28 11:17:37 -0500

    initial commit

---
+++ Deliverable2/Deliverable2/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+
+namespace FlippingCoin
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+
+            int choice;
+            Console.Write("Guess which will have more: heads or tails?");
+            string headsOrTailsGuess = Console.ReadLine();
+            if (headsOrTailsGuess.ToLower().Trim() == "heads")
+            { 
+                choice = 0;
+            }
+            else
+            { 
+                choice = 1;
+            }
+
+            Console.Write("How many times shall we flip the coin? ");
+            int numberOfFlips = int.Parse(Console.ReadLine());
+
+            // Declare variables
+            Random rand = new Random();
+            int correctCount = 0;
+            //int percentage = (correctCount / numberOfFlips) * 100;
+            //int heads = 0;
+            //int tails = 1;
+
+            for (int i = 0; i < numberOfFlips; i++)
+            {
+                int result = rand.Next(0, 2);
+
+                if (result == 0)
+                {
+                    Console.WriteLine("Heads!");
+                }
+                else
+                {
+                    Console.WriteLine("Tails!");
+                }
+                if (result == choice)
+                { //comparing result to parsed choice and incrementing the counter
+                    correctCount++;
+                }
+
+            }
+            Console.WriteLine("Your guess, " + headsOrTailsGuess + " came up " + correctCount + " time(s).");
+            int percentage = correctCount * 100 / numberOfFlips;
+            Console.WriteLine("That's " + percentage + "%");
+        }
+    }
+}
---
+++ Deliverable_one/Deliverable_one/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace Deliverable1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string password;
+            const int maxLength = 12;
+            const int minLength = 7;
+            bool hasUpperCase = false;
+            bool hasLowerCase = false;
+            bool hasExplanatory = false;
+
+            Console.WriteLine("Please enter a password with the following criteria:");
+            Console.WriteLine("At least one lowercase letter");
+            Console.WriteLine("At least one uppercase letter");
+            Console.WriteLine("At least minimum 7 characters");
+            Console.WriteLine("At least a maximum 12 characters");
+            Console.WriteLine("Must include one exclamation point(!)");
+
+            password = Console.ReadLine(); // gets password from user
+
+
+            if ((password.Length >= minLength && password.Length <= maxLength)) // checks max and min length of string inputted by user
+            {
+                if (password.Contains('!')) // check password contains '!' then,
+                    hasExplanatory = true; // hasExplanatory=true
+
+                foreach (char c in password) // read each character from password into 'c'
+                {
+                    if (char.IsUpper(c)) hasUpperCase = true; // check atleast one character upper then, set hasUpperCase=true
+                    else if (char.IsLower(c)) hasLowerCase = true; // check atleast one character lower then, set hasLowerCase=true
+                }
+
+            }
+
+            if (hasExplanatory && hasUpperCase && hasLowerCase) // check hasExplanatory and hasUpperCase and hasLowerCase then,
+                Console.WriteLine("Password is valid"); // Print password valid
+            else
+                Console.WriteLine("Password is Not valid..."); // else, password not valid
+
+        }
+    }
+}
